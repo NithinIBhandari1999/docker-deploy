7afcd409c01a51788bff7cd764c15a091eb6f797
Nithin I Bhandari <nithinibhandari2017@gmail.com>
2022-10-16 22:38:26 +0530

    Updated files

--- src/App.js
+++ src/App.js
@@ -1,16 +1,22 @@
-import React from "react";
-import logo from "./logo.svg";
-import "./App.css";
+import logo from './logo.svg';
+import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>NIB</p>
-        <p>An amazing startup idea !!.</p>
-        <p>Assignment done. LearnCodeOnline wooo</p>
-        <p>Instagram: https://instagram.com/hiteshChoudharyOfficial</p>
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
       </header>
     </div>
   );
